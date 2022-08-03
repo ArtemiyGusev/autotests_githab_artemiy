ec542a6295c33ce571953d97ebf89be655c567d6
artemiygusev04 <PMPO1002@yandex.ru>
2022-08-03 23:17:27 +0300

    test 1

--- acceptance_tests/test_github.py
+++ acceptance_tests/test_github.py
@@ -8,16 +8,21 @@ from selene.support.shared.jquery_style import s, ss
 from helpers.help_modul import url_open_size
 
 
-
-def test_one():
+@pytest.mark.parametrize("width, height",
+                         [
+                             pytest.param(1920, 1366),
+                             pytest.param(1366, 768, marks=[pytest.mark.xfail(reason="Не валидное разрешение")]),
+                             pytest.param(800, 600, marks=[pytest.mark.skip(reason="В процессе разработки")])
+                         ]
+                         )
+def test_one(width, height):
     allure.dynamic.tag("Web application")
     allure.dynamic.severity(Severity.CRITICAL)
     allure.dynamic.feature("Тесты automation-practice-form")
     allure.dynamic.story("Проверка отправленных данных в таблице через форму")
 
     with allure.step('Открываем github.com'):
-        url_open_size()
+        url_open_size(width, height)
 
     with allure.step('Клик по кнопке Sign in'):
         s('[href="/login"]').click()
-
--- helpers/help_modul.py
+++ helpers/help_modul.py
@@ -1,10 +1,11 @@
 import os
 
+import pytest
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s, ss
 
 
-def url_open_size(url='', width=1920, height=1080):
+def url_open_size(width=1920, height=1080, url=''):
     browser.config.browser_name = 'chrome'
     browser.open(url)
     browser.config.driver.set_window_size(width, height)
